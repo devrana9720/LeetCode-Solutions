24c0d3b1f2cfe03b485bccbda804d3078cc6dac4
Dev Rana <devrana3256@gmail.com>
2026-07-18 14:24:18 +0530

    Updated solution

--- Arrays/LC14_LongestCommonPrefix.java
+++ Arrays/LC14_LongestCommonPrefix.java
@@ -1,15 +1,15 @@
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        if(strs == null || strs.length == 0){
+    public String longestCommonPrefix(String[] str) {
+        if(str == null || str.length == 0){
             return "";
         }
-        for (int i = 0; i < strs[0].length(); i++) {
-            for (int j = 1; j < strs.length; j++) {
-                if (i == strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i)){
-                    return strs[0].substring(0, i);
+        for (int i = 0; i < str[0].length(); i++) {
+            for (int j = 1; j < str.length; j++) {
+                if (i == str[j].length() || str[j].charAt(i) != str[0].charAt(i)){
+                    return str[0].substring(0, i);
                 }
             }
         }
-        return strs[0];
+        return str[0];
     }
 }
--- Math/LC7_ReverseInteger.java
+++ Math/LC7_ReverseInteger.java
@@ -2,8 +2,8 @@ class Solution {
     public int reverse(int x) {
         long rev=0;
         while(x!=0){
-            int last=x%10;
-            rev=rev*10+last;
+            int num=x%10;
+            rev=rev*10+num;
             x=x/10;
             if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) {
             return 0;
