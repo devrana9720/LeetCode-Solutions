07c9f6c20656d231cb6a3b7a0311697654ec42d2
Dev Rana <devrana3256@gmail.com>
2026-07-19 16:29:25 +0530

    Changed Succesful

--- Stack/LC20_ValidParantheses.java
+++ Stack/LC20_ValidParantheses.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
-         Stack<Character> stack = new Stack<>();
+         Stack<Character> st = new Stack<>();
         for (char c : s.toCharArray()) {
             // If opening bracket, push to stack
             if (c == '(' || c == '[' || c == '{') {
-                stack.push(c);
+                st.push(c);
             } else {
                 // If stack empty, no matching opening bracket
-                if (stack.isEmpty()) return false;
-                char top = stack.pop();
+                if (st.isEmpty()) return false;
+                char top = st.pop();
                 if (c == ')' && top != '(') return false;
                 if (c == ']' && top != '[') return false;
                 if (c == '}' && top != '{') return false;
             }
         }
-        return stack.isEmpty(); // All must be matched
+        return st.isEmpty(); // All must be matched
         
     }
 }
