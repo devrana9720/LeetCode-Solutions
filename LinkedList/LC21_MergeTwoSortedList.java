647341281c7b32f6d0a0a502231fd8595378a4a7
Dev Rana <devrana3256@gmail.com>
2026-07-20 14:18:00 +0530

    Add LeetCode 21 Merge Two Sorted List

--- LinkedList/LC21_MergeTwoSortedList.java
+++ LinkedList/LC21_MergeTwoSortedList.java
@@ -12,8 +12,8 @@ class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode i=list1;
         ListNode j=list2;
-        ListNode duplicate=new ListNode(-1);
-        ListNode k=duplicate;
+        ListNode dummy=new ListNode(-1);
+        ListNode k=dummy;
         while(i!=null && j!=null){
             if(i.val<=j.val){
                 k.next=i;
@@ -31,6 +31,6 @@ class Solution {
         else{
             k.next=i;
         }
-        return duplicate.next;
+        return dummy.next;
     }
 }
