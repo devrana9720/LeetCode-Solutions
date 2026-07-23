64748e6737cf3eb736df6d2e5d011a81e3e5ec13
Dev Rana <devrana3256@gmail.com>
2026-07-23 10:42:24 +0530

    Add LeetCode 28 Find The Index Of A String

---
+++ Strings/LC28_FindIndexOfTheFirstOccurenceInAString.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        // If needle is empty, return 0
+        if (needle == null || needle.length() == 0) {
+            return 0;
+        }
+
+        int n = haystack.length();
+        int m = needle.length();
+
+        // Loop through haystack
+        for (int i = 0; i <= n - m; i++) {
+            // Check substring of length m starting at i
+            if (haystack.substring(i, i + m).equals(needle)) {
+                return i; // Found match
+            }
+        }
+        return -1; // Not found
+    }
+}
