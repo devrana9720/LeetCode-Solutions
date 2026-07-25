f1ff06639e3fae225232bf01e0085bbca10329db
Dev Rana <devrana3256@gmail.com>
2026-07-25 20:49:43 +0530

    Add LeetCode 33 Search In Rotated Sorted Array

--- Binary Search/LC33_SearchInRotatedSortedArray.java
+++ Binary Search/LC33_SearchInRotatedSortedArray.java
@@ -1,23 +1,23 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left=0;
+        int le=0;
         int right=nums.length-1;
-        while(left<=right){
-            int mid=(left+right)/2;
+        while(le<=right){
+            int mid=(le+right)/2;
             if(nums[mid]==target){
                 return mid;
             }
-            else if(nums[left]<=nums[mid]){
-                if(nums[left]<=target && target<=nums[mid]){
+            else if(nums[le]<=nums[mid]){
+                if(nums[le]<=target && target<=nums[mid]){
                     right=mid-1;
                 }
                 else{
-                    left=mid+1;
+                    le=mid+1;
                 }
             }
             else{
                 if(nums[mid]<=target && target<=nums[right]){
-                    left=mid+1;
+                    le=mid+1;
                 }
                 else{
                     right=mid-1;
