3a4eee0ede1ab3a5bf366e78809647698d002d6c
Dev Rana <devrana3256@gmail.com>
2026-07-27 13:43:57 +0530

    Changed Succesful

--- Binary Search/LC35_SearchInsertPosition.java
+++ Binary Search/LC35_SearchInsertPosition.java
@@ -1,12 +1,12 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int le = 0, right = nums.length - 1;
-        while (le <= right) {
-            int mid = le + (right - le) / 2;
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) return mid;
-            else if (nums[mid] < target) le = mid + 1;
+            else if (nums[mid] < target) left = mid + 1;
             else right = mid - 1;
         }
-        return le;
+        return left;
     }
 }
