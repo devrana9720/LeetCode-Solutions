d7c5af0efcbe5c6e86ee12e565e7fead42a026a8
Dev Rana <devrana3256@gmail.com>
2026-07-30 13:51:18 +0530

    Changed Succesful

--- Arrays/LC46_Permutation.java
+++ Arrays/LC46_Permutation.java
@@ -1,20 +1,20 @@
 class Solution {
-    public List<List<Integer>> permute(int[] num) {
+    public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        dfs(num, new boolean[num.length], new ArrayList<>(), ans);
+        dfs(nums, new boolean[nums.length], new ArrayList<>(), ans);
         return ans;
     }
 
-    private void dfs(int[] num, boolean[] used, List<Integer> path, List<List<Integer>> ans) {
-        if (path.size() == num.length) {
+    private void dfs(int[] nums, boolean[] used, List<Integer> path, List<List<Integer>> ans) {
+        if (path.size() == nums.length) {
             ans.add(new ArrayList<>(path));
             return;
         }
-        for (int i = 0; i < num.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (used[i]) continue;
             used[i] = true;
-            path.add(num[i]);
-            dfs(num, used, path, ans);
+            path.add(nums[i]);
+            dfs(nums, used, path, ans);
             path.remove(path.size() - 1);
             used[i] = false;
         }
