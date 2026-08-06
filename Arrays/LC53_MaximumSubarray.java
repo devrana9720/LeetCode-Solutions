75bc8110a9c1f3cd87743e5fbd5d4f6cc7a86a79
Dev Rana <devrana3256@gmail.com>
2026-08-06 14:54:06 +0530

    Add LeetCode Question 53 Maximum Subarray

---
+++ Arrays/LC53_MaximumSubarray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int current = nums[0];
+        int max = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            if(current + nums[i] > nums[i]){
+                current = current + nums[i];
+            } else {
+                current = nums[i];
+            }
+
+            if(current > max){
+                max = current;
+            }
+        }
+
+        return max;
+    }
+}
