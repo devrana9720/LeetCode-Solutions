829658e29d9bb55fb1c0aec492a308860296f2b0
Dev Rana <devrana3256@gmail.com>
2026-08-07 15:20:58 +0530

    Changed Succesful

--- Strings/LC58_LengthOfLastWord.java
+++ Strings/LC58_LengthOfLastWord.java
@@ -2,6 +2,7 @@ class Solution {
     public int lengthOfLastWord(String s) {
     s = s.trim();
     int lastWord = s.lastIndexOf(" ");
-    return s.length() - lastWord - 1;
+    int length = s.length() - lastWord - 1;
+    return length;
     }
 }
