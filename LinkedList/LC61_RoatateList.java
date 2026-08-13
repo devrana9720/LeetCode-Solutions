b3fdfe84e0d7fafc1062af8318c57f2b6d34f23e
Dev Rana <devrana3256@gmail.com>
2026-08-13 14:52:42 +0530

    Changed Successfull

--- LinkedList/LC61_RoatateList.java
+++ LinkedList/LC61_RoatateList.java
@@ -10,13 +10,13 @@
  */
 class Solution {
     public int length(ListNode head){
-        int len=0;
+        int length=0;
         ListNode temp=head;
         while(temp!=null){
             temp=temp.next;
-            len++;
+            length++;
         }
-        return len;
+        return length;
     }
     public ListNode rotateRight(ListNode head, int k) {
         if(head==null || head.next==null){
