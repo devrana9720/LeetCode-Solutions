5651ea75c03b1b4e841132ce13f80162eaa71edd
Dev Rana <devrana3256@gmail.com>
2026-08-18 09:34:01 +0530

    Changed SuccessFull

--- Math/LC69_Sqrt(x).java
+++ Math/LC69_Sqrt(x).java
@@ -5,16 +5,16 @@ class Solution {
         }
         int left = 1;
         int right = x;
-        int ans = 0;
+        int answer = 0;
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (mid <= x / mid) {
-                ans = mid;
+                answer = mid;
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
-        return ans;
+        return answer;
     }
 }
